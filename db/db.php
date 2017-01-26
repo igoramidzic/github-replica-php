db839285087e560156d4886f3125b8ba8eafaca2
Igor Amidzic <amidzicigor@yahoo.com>
2017-01-26 16:57:18 -0500

    changes

--- db/db.php
+++ db/db.php
@@ -1,13 +1,7 @@
 <?php
 
-// $connection = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'b961973f7f08c0', '61801897', 'heroku_2de62fcae6ffdbe');
-$connection = mysqli_connect('localhost', 'root', '', 'sipnub');
-<<<<<<< HEAD
-
-$query = "DROP TABLE sipnub-users";
-mysqli_query($connection, $query);
-=======
->>>>>>> f47da081a5dd6a5da14ad258af183bf70aaf9fcf
+$connection = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'b961973f7f08c0', '61801897', 'heroku_2de62fcae6ffdbe');
+// $connection = mysqli_connect('localhost', 'root', '', 'sipnub');
 
 if (!$connection) {
   die("Database connection error");
