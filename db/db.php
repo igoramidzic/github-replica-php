b5afb6aa5d814c0f1fe46b80b857bab960541280
Igor Amidzic <amidzicigor@yahoo.com>
2017-01-26 15:00:51 -0500

    Add cleardb

--- db/db.php
+++ db/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect('localhost', 'root', '', 'github');
+$connection = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'be120768581baf', 'a38ae27f', 'heroku_faec0df65593528');
 
 if (!$connection) {
     die("Database connection error");
