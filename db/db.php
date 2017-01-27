4187c7f1d20a33b7a3f36b4db3409ae25b790cfc
Igor Amidzic <amidzicigor@yahoo.com>
2017-01-27 10:27:14 -0500

    change production/development database settings

--- db/db.php
+++ db/db.php
@@ -1,16 +1,14 @@
 <?php
 
-echo __DIR__;
-
-if (__DIR__ == "/Applications/XAMPP/xamppfiles/htdocs/github-replica/db") {
+// If app is on production server, use cleardb database
+if (__DIR__ == "/app/db") {
+  $connection = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'b961973f7f08c0', '61801897', 'heroku_2de62fcae6ffdbe');
+} else {
+  // If server is localhost, use localhost database
   $connection = mysqli_connect('localhost', 'root', '', 'sipnub');
 }
-// elseif (__DIR__) {
-//
-// }
-$connection = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'b961973f7f08c0', '61801897', 'heroku_2de62fcae6ffdbe');
 
-
+// Check if connection is working
 if (!$connection) {
   die("Database connection error");
 }
