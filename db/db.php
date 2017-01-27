f36677f5d1666ceae9351524048566ef2ce84c2a
Igor Amidzic <amidzicigor@yahoo.com>
2017-01-27 10:24:44 -0500

    changes

--- db/db.php
+++ db/db.php
@@ -4,7 +4,8 @@ echo __DIR__;
 
 if (__DIR__ == "/Applications/XAMPP/xamppfiles/htdocs/github-replica/db") {
   $connection = mysqli_connect('localhost', 'root', '', 'sipnub');
-// } elseif (__DIR__) {
+}
+// elseif (__DIR__) {
 //
 // }
 $connection = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'b961973f7f08c0', '61801897', 'heroku_2de62fcae6ffdbe');
